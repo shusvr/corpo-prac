1f121ea2d80724548b0457c7f0bc8fb3bcbc5031
Vladimir Shustrov <shustrov.vladimir.22@yandex.ru>
2024-09-10 15:49:39 +0300

    Initial home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Stuff',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Drug Store"),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'ЭФБО-03-22 Шустров В.Р. have pushed the button this many times:',
+          children: [
+            Center(
+              child: Image.network(
+                  width: 340,
+                  "https://www.creativefabrica.com/wp-content/uploads/2019/10/24/drugs-filled-line-icon-logo-design-2-1-580x386.jpg"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Password",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Password",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.blue.shade400,
+                child: const Text("Sign In"),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: OutlinedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    shape: WidgetStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6)))),
+                child: const Text("Register"),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
