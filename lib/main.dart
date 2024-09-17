614143537a9eed2278a074961d8d710bb582faa6
Vladimir Shustrov <shustrov.vladimir.22@yandex.ru>
2024-09-17 13:05:59 +0300

    Finished home page

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stuff',
+      title: 'ЭФБО-03-22',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.purple,
       ),
       home: const HomePage(),
     );
@@ -27,61 +26,105 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Drug Store"),
-      ),
+      backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          children: [
-            Center(
-              child: Image.network(
-                  width: 340,
-                  "https://www.creativefabrica.com/wp-content/uploads/2019/10/24/drugs-filled-line-icon-logo-design-2-1-580x386.jpg"),
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(height: 80),
+            const Text(
+              'Авторизация',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
             ),
-            TextField(
+            const SizedBox(height: 120),
+            const TextField(
               decoration: InputDecoration(
-                hintText: "Password",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
+                hintText: "Логин",
+                filled: true,
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  borderSide: BorderSide.none,
+                ),
+                hintStyle: TextStyle(color: Colors.grey),
               ),
+            ),
+            const SizedBox(height: 16),
+            const TextField(
               obscureText: true,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            TextField(
               decoration: InputDecoration(
-                hintText: "Password",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(6)),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 48,
-              child: MaterialButton(
-                onPressed: () {},
-                color: Colors.blue.shade400,
-                child: const Text("Sign In"),
+                hintText: "Пароль",
+                filled: true,
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  borderSide: BorderSide.none,
+                ),
+                hintStyle: TextStyle(color: Colors.grey),
               ),
             ),
-            const SizedBox(
-              height: 16,
+            const SizedBox(height: 8),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                  value: false,
+                  onChanged: null,
+                ),
+                Text(
+                  'Запомнить меня',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+              ],
             ),
-            SizedBox(
-              width: double.infinity,
-              height: 48,
-              child: OutlinedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                    shape: WidgetStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6)))),
-                child: const Text("Register"),
+            const SizedBox(height: 12),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+              child: const Text(
+                "Войти",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  side: const BorderSide(color: Colors.blue, width: 2),
+                ),
+              ),
+              child: const Text(
+                'Регистрация',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(height: 6),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Восстановить пароль',
+                style: TextStyle(color: Colors.grey, fontSize: 18),
               ),
             ),
           ],
